16dc0e5956a23256ad386b6b8b5bbdf8d6b1768c
DeveloperLuciaH <85319857+DeveloperLuciaH@users.noreply.github.com>
2022-01-31 17:13:32 -0300

    img inicial gwenGif

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d4a9d66f8c42ab24"; }
+/******/ 		__webpack_require__.h = function() { return "510dd5cf283930b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.b05fda5d4b81589a.hot-update.js
+++ .next/static/webpack/webpack.b05fda5d4b81589a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "49fd215ef8b35107"; }
+/******/ 	__webpack_require__.h = function() { return "cdba60a44061f024"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
